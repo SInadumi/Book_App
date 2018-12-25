680b0cf8f0d83e01fe6143d7ccdfc20e6decadd3
Shun Inadumi <inazumi19@icloud.com>
2018-12-25 13:22:45 +0900

    Create User Model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   root "home#top"
   get "authors/books/:id/edit" => "books#edit"
   resources :authors
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  end
