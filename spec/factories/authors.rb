e33fe5ceb727e8659cfa89f9a377444b4db6ac11
Shun Inadumi <inazumi19@icloud.com>
2019-01-06 03:25:48 +0900

    Create test

---
+++ spec/factories/authors.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :author do
+    name "ShunInadumi"
+    gender "male"
+    email "sample99@gmail.com"
+    password "inadumi"
+    
+  end
+end
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  author = build(:author)
+  author = create(:author)
+end
